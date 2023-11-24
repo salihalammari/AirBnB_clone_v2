014889e280f04168246442abb34b523ee2ac6371
bendibdounia <bendib.dounia@gmail.com>
2023-11-24 12:36:55 -0800

    fix mysql dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,10 @@
---prepares a MySQL server for the project
+-- Create or use the database if it already exists
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
+-- Create or use the user if it already exists
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- granting all privileges to the new user
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
--- granting the SELECT privilege for the user hbnb_dev
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 -- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
